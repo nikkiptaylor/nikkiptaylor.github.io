1218b72cc33ef8023c9cb19cd543130c43abfbfe
nikkiptaylor <nicolept@stanford.edu>
2023-10-06 19:29:28 -0700

    Add github pages specific config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.node/,
-            use: 'raw-loader',
-        });
-           return config;
-     },
-}
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
+  output: "export",
 
-module.exports = nextConfig
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nikkiptaylor.github.io",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
